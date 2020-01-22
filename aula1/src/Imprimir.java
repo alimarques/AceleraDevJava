e755b067cdf87d9eddf09f761bad0469e33784b2
alimarques <ali.mrques@gmail.com>
2020-01-22 12:35:38 -0300

    Atualizacao aula1

--- aula1/src/Imprimir.java
+++ aula1/src/Imprimir.java
@@ -2,10 +2,14 @@
 public class Imprimir {
 	
 	public void imprimir(String nome, int a) {
-		System.out.println(nome+" "+a);
+		System.out.println(nome+": "+a);
 	}
 	
 	public void imprimir(String nomeA, String nomeB, int a, int b) {
-		System.out.println(nomeA +" "+ a + nomeB +" "+ b);
+		System.out.println(nomeA +": "+ a + "\n" + nomeB +": "+ b);
+	}
+	
+	public void imprimir(String nomeA, String nomeB, String nomeC, int a, int b, int c) {
+		System.out.println(nomeA +": "+ a + "\n" + nomeB +": "+ b + "\n" + nomeC +": "+ c);
 	}
 }
--- aula1/src/MainClass.java
+++ aula1/src/MainClass.java
@@ -5,21 +5,16 @@ public class MainClass {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("Hello World");
+		System.out.println("Hello World");
 			
-			//Exercicios teste = new Exercicios();
-			//teste.exercicio2A(10,20);
+		//Exercicios teste = new Exercicios();
+		//teste.exercicio2A(10,20);
 			
-			// Exercicios numericos
-			//Numericos testeNumerico = new Numericos();
-			//testeNumerico.ler();
-			//testeNumerico.area();
-			//testeNumerico.idade();
-			
-			// Desafio 1
-			Calculadora calcula = new Calculadora();
-			System.out.println(calcula.calcularSalarioLiquido(1500));
-			
+		// Exercicios numericos
+		//Numericos testeNumerico = new Numericos();
+		//testeNumerico.ler();
+		//testeNumerico.area();
+		//testeNumerico.idade();	
 			
 	}
 	
