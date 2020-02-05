602f78d842c707363a893291219a5c68a12beb16
alimarques <ali.mrques@gmail.com>
2020-02-04 22:34:07 -0300

    aula3 funcoes Lambda

--- aula3/src/aula3/AulaApplication.java
+++ aula3/src/aula3/AulaApplication.java
@@ -1,24 +1,31 @@
 package aula3;
 
 import java.util.Map;
+import java.util.function.Function;
 
 public class AulaApplication {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		// Interfaces
 		Pessoa pessoa = new Pessoa();
 		pessoa.setId(1);
 		pessoa.setNome("Ali");
 		pessoa.setSobrenome("Marques");
 		pessoa.setIdade(21);
- 
- 
+		
 		Map<String, Object> atributos = ReflectionMapper.getAttributesMap(pessoa);
  
 		for(String key : atributos.keySet()) {
 			System.out.println(key + ": " + atributos.get(key));
 		}
+		
+		
+		// Funcoes Lambda
+		Function<Integer, Integer> function = (x) -> x*3 + 2;
+		Integer resultado = function.apply(1);
+		System.out.print(resultado);
 		
 		
 	}
